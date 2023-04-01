0fd75ca8316275fd8e28a0011af1f3e5be094f89
Katherin <kathejudith@gmail.com>
2023-04-01 13:44:19 -0300

    incorporación de renderizado de productos

--- main.js
+++ main.js
@@ -7,20 +7,53 @@ const asideShoppingCar = document.getElementById('asideShoppingCar')
 
 userEmail.addEventListener('click', toggleDesktopMenu);
 function toggleDesktopMenu() {
-  let asideShoppingCarOpen = asideShoppingCar.classList.contains('inactive');
-  if (!asideShoppingCarOpen) { asideShoppingCar.classList.add('inactive') }
+  asideShoppingCar.classList.add('inactive')
   desktopMenu.classList.toggle('inactive')
 }
 burgerMenuIcon.addEventListener('click', toggleMobileMenu);
 function toggleMobileMenu() {
-  let asideShoppingCarOpen = asideShoppingCar.classList.contains('inactive');
-  if (!asideShoppingCarOpen) { asideShoppingCar.classList.add('inactive') }
+  asideShoppingCar.classList.add('inactive')
   mobileMenu.classList.toggle('inactive')
 }
 
 shoppingCarIcon.addEventListener('click', toggleAsideShoppingCar);
 function toggleAsideShoppingCar() {
-  let mobileMenuOpen = mobileMenu.classList.contains('inactive');
-  if (!mobileMenuOpen) { mobileMenu.classList.add('inactive') }
+  mobileMenu.classList.add('inactive')
+  desktopMenu.classList.add('inactive')
   asideShoppingCar.classList.toggle('inactive')
 }
+
+const productList = new Array();
+productList.push({
+  name: 'Bike',
+  price: 120,
+  img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+  name: 'Notebook',
+  price: 420,
+  img: 'https://img.freepik.com/foto-gratis/portatil-pantalla-negra-blanco-sobre-mesa-blanca_53876-97915.jpg?w=740&t=st=1680365653~exp=1680366253~hmac=ceeca9c0180a8a206016c87957cc8f6c1a8cb73fd193a3642710be4001a2eb35'
+})
+productList.push({
+  name: 'T-Shirt',
+  price: 25,
+  img: 'https://img.freepik.com/foto-gratis/camiseta-minima-anuncio-ropa_53876-104323.jpg?w=740&t=st=1680365750~exp=1680366350~hmac=8dd6ca7eadd2ad87e83aa9d88129d65e745f0dc6bf4457188cbdc2471ee67ff2,'
+})
+function renderProduct(arr){
+  for (product of arr) {
+    document.querySelector('.cards-container').innerHTML += 
+    `<div class="product-card">
+        <img src="${product.img}" alt="">
+        <div class="product-info">
+          <div>
+            <p>$${product.price}</p>
+            <p>${product.name}</p>
+          </div>
+          <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+          </figure>
+        </div>
+      </div>`
+  }
+}
+renderProduct(productList)
