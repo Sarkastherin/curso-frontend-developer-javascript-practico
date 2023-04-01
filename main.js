e8b1f55fceb95adbaabc9ba922dfa937a890ee5d
Katherin <kathejudith@gmail.com>
2023-04-01 12:01:05 -0300

    1era y 2da clase

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 const userEmail = document.getElementById('userEmail');
-const desktopMenu = document.getElementById('desktopMenu')
+const desktopMenu = document.getElementById('desktopMenu');
+const mobileMenu = document.getElementById('mobileMenu');
+const burgerMenuIcon = document.getElementById('burgerMenuIcon');
+const shoppingCarIcon = document.getElementById('shoppingCarIcon');
+const asideShoppingCar = document.getElementById('asideShoppingCar')
 
-userEmail.addEventListener('click',toggleDesktopMenu);
-function toggleDesktopMenu(){
+userEmail.addEventListener('click', toggleDesktopMenu);
+function toggleDesktopMenu() {
+  let asideShoppingCarOpen = asideShoppingCar.classList.contains('inactive');
+  if (!asideShoppingCarOpen) { asideShoppingCar.classList.add('inactive') }
   desktopMenu.classList.toggle('inactive')
+}
+burgerMenuIcon.addEventListener('click', toggleMobileMenu);
+function toggleMobileMenu() {
+  let asideShoppingCarOpen = asideShoppingCar.classList.contains('inactive');
+  if (!asideShoppingCarOpen) { asideShoppingCar.classList.add('inactive') }
+  mobileMenu.classList.toggle('inactive')
+}
+
+shoppingCarIcon.addEventListener('click', toggleAsideShoppingCar);
+function toggleAsideShoppingCar() {
+  let mobileMenuOpen = mobileMenu.classList.contains('inactive');
+  if (!mobileMenuOpen) { mobileMenu.classList.add('inactive') }
+  asideShoppingCar.classList.toggle('inactive')
 }
